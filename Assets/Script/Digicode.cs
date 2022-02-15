c877ac8e52d3406eae980be7de319bef355cdeed
Adrien <a.poignet12@orange.fr>
2022-02-15 16:58:00 +0100

    ajout code digicode

---
+++ Assets/Script/Digicode.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Digicode : MonoBehaviour
+{
+    private List<int> code = new List<int>();
+    private int[] combinaison = new int[4] {5, 7, 1, 3};
+    private bool validate;
+    [SerializeField] private TextMeshProUGUI CodePrint
+    
+    public void Digits(int id)
+    {
+        if (code.Count > 3)
+        {
+            code.RemoveAt(3);
+        }
+        code.Add(id);
+    }
+
+    public void valid_button()
+    {
+        validate = true;
+    }
+
+    public void reset()
+    {
+        code.RemoveRange(0, code.Count);
+    }
+    void Start()
+    {
+        validate = false;
+    }
+
+    void Update()
+    {
+        
+        if (validate == true)
+        {
+            if (code[0] == combinaison[0] && code[1] == combinaison[1] && code[2] == combinaison[2] && code[3] == combinaison[3])
+            {
+
+            }
+            else
+            {
+                validate = false;
+                reset();
+            }
+        }
+        if tous nombres égaux alors deplacer porte + print "VALIDE" + affichage des nombres dans un TMP
+    }
+}
