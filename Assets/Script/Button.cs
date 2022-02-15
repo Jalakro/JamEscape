d88226c3fe407136eaf4680f7e06d6307bb9d38c
baptiste <baptiste.pasquier8@gmail.com>
2022-02-15 14:31:27 +0100

    Menu ki fonctionne (enfin)

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -14,4 +14,18 @@ public class Button : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    public void Credit()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(true);
+        transform.GetChild(3).gameObject.SetActive(true);
+    }
+    public void back()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(2).gameObject.SetActive(false);
+        transform.GetChild(3).gameObject.SetActive(false);
+    }
 }
