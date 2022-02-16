06153ef4fd5bef307cec66890b6c84f2e806187f
Pasquier <bpasquier@et.esiea.fr>
2022-02-16 17:28:46 +0100

    Riddle 2 Presk Finito

--- Assets/Script/ScanValidate.cs
+++ Assets/Script/ScanValidate.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ScanValidate : MonoBehaviour
 {
+    public short ScanID;
+    [SerializeField] private SucessRiddle SuccessManager;
     // Start is called before the first frame update
     void Start()
     {
-        SucessRiddle.OnSuccesAction0 += Validation;
     }
 
-    public void Validation()
+    public void fonc (HoverEnterEventArgs args)
     {
-        transform.GetChild(0).gameObject.SetActive(true);
+        if (args.interactableObject.transform.tag == $"{ScanID}")
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+            if (ScanID == 0)
+                SuccessManager.Success0();
+            if (ScanID == 1)
+                SuccessManager.Success1();
+            if (ScanID == 2)
+                SuccessManager.Success2();
+            if (ScanID == 3)
+                SuccessManager.Success3();
+        }
+        if (args.interactableObject.transform.tag != $"{ScanID}")
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
     }
 }
